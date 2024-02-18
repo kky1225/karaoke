40686005154347b0f17a17d4e81b5635775b2a91
kky1225 <kky971225@naver.com>
2024-02-18 20:45:44 +0900

    금영미디어 노래검색 속도 개선
     - 금영미디어 노래검색 속도 약 70% 개선

--- src/main/java/com/example/karaoke/service/KYMediaService.java
+++ src/main/java/com/example/karaoke/service/KYMediaService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Slf4j
@@ -48,7 +49,7 @@ public class KYMediaService {
         return list;
     }
 
-    //TODO 시간이 너무 오래 걸리는 문제 (가수, 먼데이키즈 기준 평균 32초)
+    //TODO 시간이 너무 오래 걸리는 문제 (가수, 먼데이키즈 기준 평균 9초)
     public List<SearchSong> searchSong(String category, String keyword) {
         List<SearchSong> list = new ArrayList<>();
 
@@ -56,8 +57,41 @@ public class KYMediaService {
             StringBuilder option = getUrlParam(category, keyword);
             option.append("&s_page=");
 
-            int pageNo = 1;
+            int totalPage = getTotalPage(option);
 
+            log.debug("totalPage : {}", totalPage);
+
+            List<String> urlList = new ArrayList<>();
+
+            for(int i=1;i<=totalPage;i++) {
+                urlList.add(KY_MEDIA_URL + option + i);
+            }
+
+            urlList.stream().parallel().forEach(url -> {
+                try {
+                    Document doc2 = Jsoup.connect(url).get();
+
+                    Elements elements = doc2.select("div.search_daily_chart_wrap > ul.search_chart_list.clear:not(:first-child)");
+
+                    if(elements.get(0).childrenSize() > 0) {
+                        for(Element e : elements) {
+                            list.add(
+                                    SearchSong.builder()
+                                            .no(e.select("li.search_chart_num").html())
+                                            .title(e.select("span.tit").get(0).html())
+                                            .singer(e.select("li.search_chart_sng").html())
+                                            .lyrics(e.select("li.search_chart_cmp").html())
+                                            .music(e.select("li.search_chart_wrt").html())
+                                            .build()
+                            );
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("파싱 중 오류 발생!");
+                }
+            });
+
+            /*
             while (true) {
                 Document doc = Jsoup.connect(KY_MEDIA_URL + option + pageNo++).get();
 
@@ -83,11 +117,17 @@ public class KYMediaService {
                     break;
                 }
             }
+             */
         }catch (Exception e) {
             System.out.println("파싱 중 오류 발생!");
         }
 
-        return list;
+        return list.stream().sorted(new Comparator<SearchSong>() {
+            @Override
+            public int compare(SearchSong o1, SearchSong o2) {
+                return o1.getTitle().compareTo(o2.getTitle());
+            }
+        }).toList();
     }
 
     private StringBuilder getUrlParam(String category, String keyword) {
@@ -104,4 +144,29 @@ public class KYMediaService {
         return option.append("/?category=").append(type)
                 .append("&keyword=").append(keyword);
     }
+
+    private int getTotalPage(StringBuilder option) {
+        int totalPage = 0;
+
+        try {
+            int pageNo = 1;
+
+            Document doc = Jsoup.connect(KY_MEDIA_URL + option + pageNo++).get();
+
+            int total = Integer.parseInt(doc.select("p.search_tit > span").text());
+
+            log.debug("total : {}", total);
+
+            totalPage = total / 15;
+
+            if(total % 15 > 0) {
+                totalPage++;
+            }
+        }catch (Exception e) {
+            log.debug("totalPage Error");
+        }
+
+        return totalPage;
+    }
 }
+
